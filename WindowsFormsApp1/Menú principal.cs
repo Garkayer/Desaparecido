66a7541a35aece5604cb0076f1de5fe365011b2f
JosèCarlos <JosCarlos@XENON>
2020-05-24 18:35:53 -0600

    Otros toques

---
+++ WindowsFormsApp1/Menú principal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Program.Principal.Hide();
+            Desaparecido Desaparecido = new Desaparecido();
+            Desaparecido.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Program.Principal.Hide();
+            Entrevistado Entrevistado = new Entrevistado();
+            Entrevistado.Show();
+        }
+    }
+}
--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -11,12 +11,13 @@ namespace WindowsFormsApp1
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
+        public static Principal Principal;
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Desaparecido());
+            Application.Run(new Principal());
         }
     }
 }
